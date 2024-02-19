855f0a6f64e4d87b400bbcd18ce9664c9c5c25f0
Sholom Morgenstern <sholommorgenstern@gmail.com>
2024-02-19 13:27:22 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">
+        that's all folks
+      </h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
